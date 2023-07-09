db5aa44bb3de66cb44035853738a475de87d2bbd
sabriflorin <sabriflorin@gmail.com>
2023-07-09 20:23:48 +0200

    delete install

--- install.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-set /p ABLETON=< settings.txt
-echo %CD%\Options.txt copy to %AppData%\Ableton\Live %ABLETON%\Preferences\Options.txt
-copy "%CD%\Options.txt" "%AppData%\Ableton\Live %ABLETON%\Preferences\Options.txt"
-%SystemRoot%\explorer.exe "%AppData%\Ableton\Live %ABLETON%\Preferences\"
-#pause
-exit
