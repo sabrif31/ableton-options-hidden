baaf4fee5f7d6633f63a76e4bfdf1c6646344c85
sabriflorin <sabriflorin@gmail.com>
2023-07-09 20:20:19 +0200

    Create uninstall.bat

---
+++ uninstall.bat
@@ -0,0 +1,6 @@
+@echo off
+set /p ABLETON=< settings.txt
+del "%AppData%\Ableton\Live %ABLETON%\Preferences\Options.txt" /f
+echo Fichier supprimé %AppData%\Ableton\Live %ABLETON%\Preferences\Options.txt
+#pause
+exit
