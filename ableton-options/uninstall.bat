cbafa84182cccc48a4c62ad65eea0efbac56e755
sabriflorin <sabriflorin@gmail.com>
2023-07-09 20:45:56 +0200

    Update uninstall.bat

--- ableton-options/uninstall.bat
+++ ableton-options/uninstall.bat
@@ -1,7 +1,5 @@
 @echo off
-:: set /p ABLETON=Enter Version ableton:
 set /p ABLETON=< settings.txt
-
 del "%AppData%\Ableton\Live %ABLETON%\Preferences\Options.txt" /f
 echo Fichier supprimé %AppData%\Ableton\Live %ABLETON%\Preferences\Options.txt
 #pause
